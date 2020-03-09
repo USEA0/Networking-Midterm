dd8e811599d3b20286e5d1ff96d95e80019e6300
John <john.wang@uoit.net>
2020-03-09 14:44:05 -0400

    physics polish

--- Midterm Project/Assets/Puck.cs
+++ Midterm Project/Assets/Puck.cs
@@ -11,6 +11,8 @@ public class Puck : MonoBehaviour
     //velocity
     public Vector2 V;
 
+    public float maxSpeed;
+    public float minSpeed;
     //drag
     public float drag;
 
@@ -25,24 +27,49 @@ public class Puck : MonoBehaviour
             V += impulse;
         }
 
-        //reset impulse
-        I.Clear();
+        if (V.magnitude > maxSpeed) {
+            V = V.normalized * maxSpeed;
+        }
+        if (V.magnitude < minSpeed)
+        {
+            V = new Vector2(0, 0);
+        }
+            //reset impulse
+            I.Clear();
 
         //update position
         this.transform.position = new Vector2(this.transform.position.x + V.x, this.transform.position.y + V.y);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
 
         //collision handling
         if (collision.gameObject.tag == "HorizWall")
         {
             V.y = -V.y;
+            if (this.transform.position.y < -3.8f)
+            {
+                this.transform.position = new Vector2(this.transform.position.x, -3.8f);
+            }
+            else if (this.transform.position.y > 3.75f)
+            {
+                this.transform.position = new Vector2(this.transform.position.x, 3.75f);
+            }
+
         }
         else if (collision.gameObject.tag == "VertWall")
         {
             V.x = -V.x;
+            if (this.transform.position.x < -7.9f)
+            {
+                this.transform.position = new Vector2( -7.9f, this.transform.position.y);
+            }
+            else if (this.transform.position.x > 7.9f)
+            {
+                this.transform.position = new Vector2( 7.9f, this.transform.position.y);
+            }
+
         }
         else if (collision.gameObject.tag == "Player")
         {
@@ -51,7 +78,7 @@ public class Puck : MonoBehaviour
                 Debug.Log("Triggered");
 
                 //calculate impulse
-                Vector2 impulseTemp = -collision.gameObject.GetComponent<Paddle>().velo; //(collision.transform.position - this.transform.position).normalized * ;
+                Vector2 impulseTemp = -collision.gameObject.GetComponent<Paddle>().velo;
 
                 //queue impulse locally
                 I.Enqueue(impulseTemp);
@@ -59,7 +86,19 @@ public class Puck : MonoBehaviour
                 //send to the network
                 NetworkManager.SendImpulse(impulseTemp);
             }
+            //manual collision
+            float dx = this.transform.position.x - collision.transform.position.x;
+            float dy = this.transform.position.y - collision.transform.position.y;
 
+            var distance = Mathf.Sqrt(dx * dx + dy * dy);
+
+            if (distance < this.transform.localScale.y + collision.transform.localScale.y)
+            {
+                Vector3 dir = -((collision.transform.position - this.transform.position).normalized);
+                float nDist = (this.transform.localScale.y + collision.transform.localScale.y - distance);
+
+                this.transform.position = this.transform.position + (dir * (nDist + 0.01f));
+            }
         }
         else if (collision.gameObject.tag == "GoalR")
         {
@@ -77,19 +116,7 @@ public class Puck : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            //manual collision
-            float dx = this.transform.position.x - collision.transform.position.x;
-            float dy = this.transform.position.y - collision.transform.position.y;
 
-            var distance = Mathf.Sqrt(dx * dx + dy * dy);
-
-            if (distance < this.transform.localScale.y + collision.transform.localScale.y)
-            {
-                Vector3 dir = -((collision.transform.position - this.transform.position).normalized);
-                float nDist = (this.transform.localScale.y + collision.transform.localScale.y - distance);
-
-                this.transform.position = this.transform.position + (dir * (nDist+0.01f));
-            }
 
         }
     }
